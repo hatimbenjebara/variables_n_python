f4218bd5534584bf5cc09aada58be2ac9539c72b
Hatim Benjebara <108321950+hatimbenjebara@users.noreply.github.com>
2023-05-01 11:19:35 +0100

    Add files via upload

---
+++ variable_in_python.py
@@ -0,0 +1,30 @@
+#declartion of variable a, b, c, x, y, z
+a ='3' #define it as string
+b = 3 # define as integer
+c =3.0 #define as float
+x= str(3) # use str() method to define a string
+y =int(3) # use int() method to define a integer
+z = float(3) #use float() method to define a float
+print(type(a)) # print the type of a using type() method
+print(type(b))
+print(type(c))
+print(type(x))
+print(type(y))
+print(type(z))
+#assign multiple variable in one line 
+x , y, z = "hatim", "maha", "zakaria" 
+print(x)
+print(y)
+print(z)
+print( x, y, z)
+countries = ["algeria", "France", "Morocco"]
+x, y, z = countries # by default x= countries[0], y = countries[1], z = countries[2]
+print(x)
+print(y)
+print(z)
+x=str(c) # convert float to string
+y= float(b) # convert int to float
+z= int(a) # convert string to int
+print(x)
+print(y)
+print(z)
